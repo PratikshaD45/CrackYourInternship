a65d19630e9e87c9234f37280e73381366c503b6
Pratiksha <pratikshadarekar240@gmail.com>
2024-07-15 23:09:00 +0530

    .

--- Array/RemoveDuplicatesfromSortedArray.java
+++ Array/RemoveDuplicatesfromSortedArray.java
@@ -2,18 +2,14 @@ import java.util.*;
 
 class Solution {
     public int removeDuplicates(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            set.add(nums[i]);
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
+            }
         }
-        Iterator<Integer> iterator = set.iterator();
-        int i = 0;
-        while (iterator.hasNext()) {
-            int element = iterator.next();
-            nums[i] = element;
-            i++;
-        }
-        return set.size();
+        return j;
 
     }
 }
